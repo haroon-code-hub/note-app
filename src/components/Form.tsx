852f7de7aed58ef97c5fcb0fafc08b398d79b5bf
haroonsaeed82096 <30671721+haroon-code-hub@users.noreply.github.com>
2025-12-05 21:31:25 +0000

    make form collapsible

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -14,6 +14,8 @@ export default function Form({ notes, setNotes }: FormProps) {
     description: "",
   });
 
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
   function handleChange(e: { target: { value: string; name: string } }) {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   }
@@ -34,64 +36,71 @@ export default function Form({ notes, setNotes }: FormProps) {
   }
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="title" className="block">
-            Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            className="border border-black rounded-lg"
-            value={formData.title}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="priority" className="block">
-            Priority
-          </label>
-          <select
-            name="priority"
-            className="border border-black rounded-lg"
-            value={formData.priority}
-            onChange={handleChange}
-          >
-            <option value="high">High</option>
-            <option value="medium">Medium</option>
-            <option value="low">Low</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="category" className="block">
-            Category
-          </label>
-          <select
-            name="category"
-            className="border border-black rounded-lg"
-            value={formData.category}
-            onChange={handleChange}
-          >
-            <option value="Work">Work</option>
-            <option value="Personal">Personal</option>
-            <option value="Ideas">Ideas</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="description" className="block">
-            Description
-          </label>
-          <textarea
-            name="description"
-            className="border border-black rounded-lg"
-            value={formData.description}
-            onChange={handleChange}
-          ></textarea>
-        </div>
-        <button className="bg-blue-600 py-2 px-6 border rounded-md cursor-pointer hover:bg-blue-500">
-          Submit
-        </button>
-      </form>
+      <button onClick={() => setIsFormVisible(!isFormVisible)}>
+        {isFormVisible ? "Hide Form X" : "Add new Note +"}
+      </button>
+      {isFormVisible && (
+        <>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="title" className="block">
+                Title
+              </label>
+              <input
+                type="text"
+                name="title"
+                className="border border-black rounded-lg"
+                value={formData.title}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="priority" className="block">
+                Priority
+              </label>
+              <select
+                name="priority"
+                className="border border-black rounded-lg"
+                value={formData.priority}
+                onChange={handleChange}
+              >
+                <option value="high">High</option>
+                <option value="medium">Medium</option>
+                <option value="low">Low</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="category" className="block">
+                Category
+              </label>
+              <select
+                name="category"
+                className="border border-black rounded-lg"
+                value={formData.category}
+                onChange={handleChange}
+              >
+                <option value="Work">Work</option>
+                <option value="Personal">Personal</option>
+                <option value="Ideas">Ideas</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="description" className="block">
+                Description
+              </label>
+              <textarea
+                name="description"
+                className="border border-black rounded-lg"
+                value={formData.description}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+            <button className="bg-blue-600 py-2 px-6 border rounded-md cursor-pointer hover:bg-blue-500">
+              Submit
+            </button>
+          </form>
+        </>
+      )}
     </>
   );
 }
